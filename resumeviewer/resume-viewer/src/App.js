4140f468fcb92e771d8a93610221033750dc2f75
Eric Loos <ericloos00@gmail.com>
2017-07-05 16:16:55 -0500

    setup components, style sheet

---
+++ resumeviewer/resume-viewer/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Footer from './Components/Footer';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
+import Resume from './Components/Resume';
+import Testimonials from './Components/Testimonials';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <About/>
+        <Resume/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
