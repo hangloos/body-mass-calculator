9af78e748f69f9a4b93707e217ee95bd89522ea8
Eric Loos <ericloos00@gmail.com>
2017-07-06 09:10:56 -0500

    added dynamic for half

--- resumeviewer/resume-viewer/src/App.js
+++ resumeviewer/resume-viewer/src/App.js
@@ -7,22 +7,46 @@ import Contact from './Components/Contact';
 import Portfolio from './Components/Portfolio';
 import Resume from './Components/Resume';
 import Testimonials from './Components/Testimonials';
+import $ from 'jquery';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      foo: 'bar'
+      foo: 'bar',
+      resumeData:{}
     }
   }
+
+  getResumeData(){
+    $.ajax({
+      url: 'http://localhost:3000/resumeData.json',
+      data: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({
+          resumeData: data
+        })
+      }.bind(this),
+      error: function(xhr, status, error){
+        console.log(error);
+        alert(error);
+      }
+    })
+  }
+
+
+  componentDidMount(){
+    this.getResumeData();
+  }
   render() {
-    
+    console.log(this.state.resumeData)
     return (
       <div className="App">
-        <Header/>
-        <About/>
-        <Resume/>
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        <Resume data={this.state.resumeData.resume}/>
         <Portfolio/>
         <Testimonials/>
         <Contact/>
