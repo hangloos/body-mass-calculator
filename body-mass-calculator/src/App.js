c73947304d9cf2ab2c9641e34b1d742a128e73ec
Eric Loos <ericloos00@gmail.com>
2017-07-07 12:38:46 -0500

    height is changing based on sliding

--- body-mass-calculator/src/App.js
+++ body-mass-calculator/src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Range from './Components/Range'
+import Output from './Components/Output'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      height: 170,
+      weight: 65,
+      bmi: 22.49,
+      bmiClass: 'Normal'
+    }
+  }
+
+  heightChange(height){
+    this.setState({
+      height: height
+    }, function(){
+      console.log(this.state)
+    })
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Body Mass Calculator</h2>
+          </div>
+          <form>
+            <div>
+              <label>Height</label>
+              <Range value={this.state.height} onChange={this.heightChange.bind(this)}/>
+            </div>
+            <div>
+              <label>Weight</label>
+              <Range/>
+            </div>
+          </form><br/><br/><br/>
+          <Output/>
       </div>
     );
   }
